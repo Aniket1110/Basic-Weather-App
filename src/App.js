fa1255c9c1048ab0d3a7649395919dd168a773f7
Aniket Majhi <aniketmajhi2001@gmail.com>
2021-11-16 22:48:58 +0530

    added basic UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import Header from "./components/Header";
+import Mysvg from './wave.svg'
+
+const backgroundStyle = {
+  backgroundImage: `url(${Mysvg})`,
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  width: '100vw',
+  height: '100vh'
+};
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App bg-dark" style={backgroundStyle}>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="bg-dark text-center p-4 text-light fs-2">
+                Weather Check
+            </div>
+            <div className="row py-3 ">
+                <div className="col-2 text-center text-light fs-3">
+                    <Icon icon="ci:location" />
+                    Kolkata,IN
+                </div>
+                <div className="col-8 text-center text-light">
+                    <Icon icon="ant-design:search-outlined" className="fs-3"/>
+                    <input type="search" placeholder="search..." className="px-3 py-1 rounded" autoFocus />
+                </div>
+                <div className="col-2">
+                    <i className="fas fa-cloud-sun text-secondary " style={{ fontSize: "150px" }}></i>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Header
